ec06ee78bb10a0bc177d3371747338253188d404
Jonathan Simon <nfernal13@outlook.com>
2017-09-08 08:38:57 -0700

    init

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Maduro Engineering'
+maintainer_email 'nfernal13@outlook.com'
+license 'mit'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'mediawiki_cookbook'
+maintainer 'Maduro Engineering'
+maintainer_email 'nfernal13@outlook.com'
+license 'MIT'
+description 'Installs/Configures mediawiki_cookbook'
+long_description 'Installs/Configures mediawiki_cookbook'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/mediawiki_cookbook/issues'
+
+# The `source_url` points to the development reposiory for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/mediawiki_cookbook'
